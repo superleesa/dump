3e64654e11b26fc699657f2613ed3b344dab4ab2
Satoshi <skas0016@student.monash.edu>
2024-01-16 06:43:15 +0900

    Fix minor error

--- data_loader.py
+++ data_loader.py
@@ -7,16 +7,14 @@ def generate_prompt(data_record):
     else:
         result = f'### 指示:{data_record["instruction"]}\n回答:{data_record["output"]}'
 
-    data_record["text"] = result  # mutating
-    return data_record
+    return {"text": result}
 
 
 def load_my_dataset(cutoff_length=512):
     dataset_name = "kunishou/databricks-dolly-15k-ja"
     dataset = load_dataset(dataset_name)
-    dataset = dataset["train"].map(generate_prompt)
-    dataset = dataset["train"].filter(lambda row: len(row["text"]) < cutoff_length).shuffle()
+    dataset = dataset.map(generate_prompt)
+    dataset = dataset.filter(lambda row: len(row["text"]) < cutoff_length).shuffle()
     dataset = dataset["train"].train_test_split(test_size=2000)
     train_dataset, eval_dataset = dataset["train"], dataset["test"]
     return train_dataset, eval_dataset
-    
